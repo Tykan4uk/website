c447a5fb2f857270c2e1642e712d7ea3dfcaeae9
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-06-28 14:08:41 +0300

    Adding GameEntity

---
+++ WebSite/WebSite/Models/GameEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WebSite.Models
+{
+    public class GameEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Developer { get; set; }
+        public string Publisher { get; set; }
+        public string Genre { get; set; }
+        public DateTime Release { get; set; }
+    }
+}
