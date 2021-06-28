f9369d5fda4cd03684999b71ffeb41229dd0e887
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-06-28 14:09:55 +0300

    Adding ICatalogService and CatalogService

---
+++ WebSite/WebSite/Services/Abstractions/ICatalogService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using System.Collections.Generic;
+using WebSite.Models;
+
+namespace WebSite.Services.Abstractions
+{
+    public interface ICatalogService
+    {
+        Task<List<GameEntity>> GetByPage(int page);
+    }
+}
---
+++ WebSite/WebSite/Services/CatalogService.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using System.Net.Http;
+using System.Collections.Generic;
+using Microsoft.Extensions.Options;
+using WebSite.Services.Abstractions;
+using WebSite.Models;
+using WebSite.Configurations;
+
+namespace WebSite.Services
+{
+    public class CatalogService : ICatalogService
+    {
+        private readonly IHttpClientService _httpClientService;
+        private readonly RouteConfig _routeConfig;
+
+        public CatalogService(IHttpClientService httpClientService, IOptions<RouteConfig> options)
+        {
+            _routeConfig = options.Value;
+            _httpClientService = httpClientService;
+        }
+
+        public async Task<List<GameEntity>> GetByPage(int page)
+        {
+            string url = $"{_routeConfig.GetByPageUrl}?page={page}";
+            var response = await _httpClientService.SendAsync<List<GameEntity>>(url, HttpMethod.Get);
+            return response;
+        }
+    }
+}
