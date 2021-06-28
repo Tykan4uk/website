05cc8c57462b3ee99f63ab48ae1e683daba2818e
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-06-28 14:14:50 +0300

    HomeContoller: adding method TryGetOutput and view of this method

--- WebSite/WebSite/Controllers/HomeController.cs
+++ WebSite/WebSite/Controllers/HomeController.cs
@@ -1,21 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using WebSite.Models;
+using WebSite.Services.Abstractions;
 
 namespace WebSite.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ICatalogService _catalogService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ICatalogService catalogService)
         {
             _logger = logger;
+            _catalogService = catalogService;
         }
 
         public IActionResult Index()
@@ -32,6 +31,12 @@ namespace WebSite.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult TryGetOutput()
+        {
+            var response = _catalogService.GetByPage(0).GetAwaiter().GetResult();
+            return View(response);
         }
     }
 }
