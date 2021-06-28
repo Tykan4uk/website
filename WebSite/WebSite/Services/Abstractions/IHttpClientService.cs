80e588e306c891aa6bb7380ee9a1110006cb66a1
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-06-28 14:09:08 +0300

    Adding IHttpClientService and HttpClientService

---
+++ WebSite/WebSite/Services/Abstractions/IHttpClientService.cs
@@ -0,0 +1,11 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WebSite.Services.Abstractions
+{
+    public interface IHttpClientService
+    {
+        Task<TResponse> SendAsync<TResponse>(string url, HttpMethod method, object content = null)
+            where TResponse : class;
+    }
+}
---
+++ WebSite/WebSite/Services/HttpClientService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using WebSite.Services.Abstractions;
+
+namespace WebSite.Services
+{
+    public class HttpClientService : IHttpClientService
+    {
+        private readonly IHttpClientFactory _clientFactory;
+
+        public HttpClientService(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+
+        public async Task<TResponse> SendAsync<TResponse>(string url, HttpMethod method, object content = null)
+            where TResponse : class
+        {
+            var client = _clientFactory.CreateClient();
+
+            var httpMessage = new HttpRequestMessage();
+            httpMessage.RequestUri = new Uri(url);
+            httpMessage.Method = method;
+
+            if (content != null)
+            {
+                httpMessage.Content = new StringContent(JsonConvert.SerializeObject(content), Encoding.UTF8, "application/json");
+            }
+
+            var result = await client.SendAsync(httpMessage);
+
+            if (result.IsSuccessStatusCode)
+            {
+                var resultContent = await result.Content.ReadAsStringAsync();
+                var response = JsonConvert.DeserializeObject<TResponse>(resultContent);
+                return response;
+            }
+
+            return null;
+        }
+    }
+}
