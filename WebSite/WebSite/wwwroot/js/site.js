0d502b5341eeed3e24ad4b8042cf9afc4e591812
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-16 09:03:00 +0300

    site.js: adding new function

--- WebSite/WebSite/wwwroot/js/site.js
+++ WebSite/WebSite/wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function addToCartJs(id) {
+
+    $.post("/Cart/AddToCart", { gameId: id })
+}
+
+function deleteFromCartJs(id) {
+
+    $.post("/Cart/RemoveFromCart", { gameId: id })
+}
