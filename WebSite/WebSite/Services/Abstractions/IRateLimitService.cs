4f1cf83d41c9dd89eaad6da15a684065d8b2d660
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-16 08:59:16 +0300

    Adding IRateLimitService

---
+++ WebSite/WebSite/Services/Abstractions/IRateLimitService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using WebSite.Models.Responses;
+
+namespace WebSite.Services.Abstractions
+{
+    public interface IRateLimitService
+    {
+        Task<CheckRateLimitResponse> CheckRateLimit(string name);
+    }
+}
