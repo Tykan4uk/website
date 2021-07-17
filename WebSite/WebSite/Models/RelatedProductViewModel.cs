ea5ab8181267b41b5ee3cb526e1370863661267c
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-17 11:18:48 +0300

    Adding RelatedProductViewModel

---
+++ WebSite/WebSite/Models/RelatedProductViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WebSite.Models
+{
+    public class RelatedProductViewModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
