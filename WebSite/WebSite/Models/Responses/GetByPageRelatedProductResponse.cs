31621c6d14454ab8ab8c6da77c76171008f67ad0
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-17 11:14:02 +0300

    Adding GetByPageRelatedProductResponse

---
+++ WebSite/WebSite/Models/Responses/GetByPageRelatedProductResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace WebSite.Models.Responses
+{
+    public class GetByPageRelatedProductResponse
+    {
+        public IEnumerable<RelatedProductViewModel> RelatedProducts { get; set; }
+        public int TotalRecords { get; set; }
+    }
+}
