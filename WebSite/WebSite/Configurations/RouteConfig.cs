1b59875f168a781a795ca11acc3736774a76e979
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-20 22:10:53 +0300

    RouteConfig: change order of properties

--- WebSite/WebSite/Configurations/RouteConfig.cs
+++ WebSite/WebSite/Configurations/RouteConfig.cs
@@ -3,8 +3,8 @@
     public class RouteConfig
     {
         public string Game { get; set; }
+        public string RelatedProduct { get; set; }
         public string RateLimit { get; set; }
         public string Cart { get; set; }
-        public string RelatedProduct { get; set; }
     }
 }
