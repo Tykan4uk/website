f213c7be072452245b1cba3eb35031d68a5c939e
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-25 22:45:46 +0300

    Adding GameByPageGameResponse

---
+++ WebSite/WebSite/Models/Responses/GetByPageGameResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace WebSite.Models.Responses
+{
+    public class GetByPageGameResponse
+    {
+        public IEnumerable<GameModel> Games { get; set; }
+        public int TotalRecords { get; set; }
+    }
+}
