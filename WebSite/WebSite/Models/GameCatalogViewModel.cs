aca22f52cfa6bf29b9d74b8782b65d549ecdf3f0
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-25 23:15:45 +0300

    Adding GameCatalogViewModel

---
+++ WebSite/WebSite/Models/GameCatalogViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using WebSite.Common.Enums;
+
+namespace WebSite.Models
+{
+    public class GameCatalogViewModel
+    {
+        public IEnumerable<GameModel> Games { get; set; }
+        public int TotalRecords { get; set; }
+        public int CurrentPage { get; set; }
+        public int PageSize { get; set; }
+        public SortedTypeEnum SortedType { get; set; }
+    }
+}
