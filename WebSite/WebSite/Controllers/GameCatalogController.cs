934e51fa91f796cf672c7625f3f7f54baab393c6
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-25 23:19:56 +0300

    GameCatalogController: adding new logic to method Catalog

--- WebSite/WebSite/Controllers/GameCatalogController.cs
+++ WebSite/WebSite/Controllers/GameCatalogController.cs
@@ -1,6 +1,11 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using WebSite.Common.Enums;
+using WebSite.Configurations;
+using WebSite.Models;
+using WebSite.Models.Requests;
 using WebSite.Services.Abstractions;
 
 namespace WebSite.Controllers
@@ -8,18 +13,37 @@ namespace WebSite.Controllers
     public class GameCatalogController : Controller
     {
         private readonly ILogger<GameCatalogController> _logger;
-        private readonly ICatalogService _catalogService;
+        private readonly IGameService _catalogService;
+        private readonly WebSiteConfig _options;
 
-        public GameCatalogController(ILogger<GameCatalogController> logger, ICatalogService catalogService)
+        public GameCatalogController(
+            ILogger<GameCatalogController> logger,
+            IOptions<Config> options,
+            IGameService catalogService)
         {
             _logger = logger;
+            _options = options.Value.WebSiteConfig;
             _catalogService = catalogService;
         }
 
-        public async Task<IActionResult> Catalog(int page)
+        public async Task<IActionResult> Catalog(int page, SortedTypeEnum sortedType)
         {
-            var response = await _catalogService.GetByPage(page);
-            return View(response);
+            var request = new GetByPageProductRequest() { Page = page, PageSize = _options.PageSize, SortedType = sortedType };
+            var response = await _catalogService.GetByPage(request);
+            if (response != null)
+            {
+                var view = new GameCatalogViewModel()
+                {
+                    Games = response.Games,
+                    TotalRecords = response.TotalRecords,
+                    CurrentPage = page,
+                    PageSize = _options.PageSize,
+                    SortedType = sortedType
+                };
+                return View(view);
+            }
+
+            return View("you died!");
         }
     }
 }
