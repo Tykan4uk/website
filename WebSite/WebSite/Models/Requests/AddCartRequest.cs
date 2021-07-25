797739c558b5fe2fe6af9af319590f240d246422
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-25 23:14:59 +0300

    Adding AddCartRequest

---
+++ WebSite/WebSite/Models/Requests/AddCartRequest.cs
@@ -0,0 +1,14 @@
+﻿using WebSite.Common.Enums;
+
+namespace WebSite.Models.Requests
+{
+    public class AddCartRequest
+    {
+        public string UserId { get; set; }
+        public string ProductId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public ProductTypeEnum Type { get; set; }
+    }
+}
