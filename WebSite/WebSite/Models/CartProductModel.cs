89ebaf756b97adf932ff9b8f06e66c8fe65675ad
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-25 23:15:22 +0300

    Adding CartProductModel

---
+++ WebSite/WebSite/Models/CartProductModel.cs
@@ -0,0 +1,13 @@
+﻿using WebSite.Common.Enums;
+
+namespace WebSite.Models
+{
+    public class CartProductModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public ProductTypeEnum Type { get; set; }
+    }
+}
