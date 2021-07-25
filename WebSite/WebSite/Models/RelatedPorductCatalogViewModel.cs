94b3a07e8e3f98304ff382b49de7e4f88c146e92
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-25 22:34:15 +0300

    RelatedProductViewModel => RelatedProductCatalogViewModel

---
+++ WebSite/WebSite/Models/RelatedPorductCatalogViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using WebSite.Common.Enums;
+
+namespace WebSite.Models
+{
+    public class RelatedPorductCatalogViewModel
+    {
+        public IEnumerable<RelatedProductModel> RelatedProducts { get; set; }
+        public int TotalRecords { get; set; }
+        public int CurrentPage { get; set; }
+        public int PageSize { get; set; }
+        public SortedTypeEnum SortedType { get; set; }
+    }
+}
