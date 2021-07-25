76ad8887a2a06758d55dd86cb6a2f72c3d9624ab
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-25 22:33:38 +0300

    Adding RelatedProductModel

---
+++ WebSite/WebSite/Models/RelatedProductModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WebSite.Models
+{
+    public class RelatedProductModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
