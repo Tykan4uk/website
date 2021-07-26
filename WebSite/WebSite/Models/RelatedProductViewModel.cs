0504cc34e6981d62facc4a3ba5f44d7c47d829f3
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-26 11:32:44 +0300

    Adding RelatedProductViewModel

---
+++ WebSite/WebSite/Models/RelatedProductViewModel.cs
@@ -0,0 +1,11 @@
+﻿using WebSite.Common.Enums;
+
+namespace WebSite.Models
+{
+    public class RelatedProductViewModel
+    {
+        public RelatedProductModel RelatedProduct { get; set; }
+        public int Page { get; set; }
+        public SortedTypeEnum SortedType { get; set; }
+    }
+}
