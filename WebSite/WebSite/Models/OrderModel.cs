574e1b95b43a165a214c754485a00ba40e34b43b
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-26 19:30:49 +0300

    Adding OrderModel

---
+++ WebSite/WebSite/Models/OrderModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WebSite.Models
+{
+    public class OrderModel
+    {
+        public string Id { get; set; }
+        public string UserId { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
