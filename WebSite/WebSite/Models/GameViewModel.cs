55bb7aa45e9191565422e03fec789e6e21a499a9
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-26 11:32:27 +0300

    GameViewModel: adding properties Page and SortedType

---
+++ WebSite/WebSite/Models/GameViewModel.cs
@@ -0,0 +1,11 @@
+﻿using WebSite.Common.Enums;
+
+namespace WebSite.Models
+{
+    public class GameViewModel
+    {
+        public GameModel Game { get; set; }
+        public int Page { get; set; }
+        public SortedTypeEnum SortedType { get; set; }
+    }
+}
