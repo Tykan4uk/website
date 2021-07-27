600966d4f9992dbda730f3a127057ca121827997
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-27 12:44:01 +0300

    Adding GetByIdOrderViewModel

---
+++ WebSite/WebSite/Models/GetByIdOrderViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace WebSite.Models
+{
+    public class GetByIdOrderViewModel
+    {
+        public string OrderId { get; set; }
+        public List<CartProductModel> Products { get; set; }
+    }
+}
