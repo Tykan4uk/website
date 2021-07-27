d48ed49d6c5baa554c351af17d7e3909c15dcc40
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-27 12:44:23 +0300

    CartController: new logic

--- WebSite/WebSite/Controllers/CartController.cs
+++ WebSite/WebSite/Controllers/CartController.cs
@@ -1,35 +1,70 @@
 ﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using WebSite.Common.Enums;
+using WebSite.Models.Requests;
 using WebSite.Services.Abstractions;
 
 namespace WebSite.Controllers
 {
+    [Authorize]
     public class CartController : Controller
     {
-        private readonly ILogger<CatalogController> _logger;
+        private readonly ILogger<GameCatalogController> _logger;
         private readonly ICartService _cartService;
 
-        public CartController(ILogger<CatalogController> logger, ICartService cartService)
+        public CartController(ILogger<GameCatalogController> logger, ICartService cartService)
         {
             _logger = logger;
             _cartService = cartService;
         }
 
-        public async Task AddToCart(int gameId)
+        public async Task AddToCart(string productId, string name, string description, decimal price, ProductTypeEnum type)
         {
-            await _cartService.AddToCart(1, gameId);
+            var userId = User.FindFirst("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier").Value;
+            var request = new AddCartRequest()
+            {
+                UserId = userId,
+                ProductId = productId,
+                Name = name,
+                Description = description,
+                Price = price,
+                Type = type
+            };
+            await _cartService.AddToCart(request);
         }
 
         public async Task<IActionResult> GetCart()
         {
-            var response = await _cartService.GetCart(1);
+            var userId = User.FindFirst("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier").Value;
+            var request = new GetCartRequest()
+            {
+                UserId = userId
+            };
+            var response = await _cartService.GetCart(request);
             return View(response);
         }
 
-        public async Task RemoveFromCart(int gameId)
+        public async Task RemoveFromCart(string productId)
         {
-            await _cartService.RemoveFromCart(1, gameId);
+            var userId = User.FindFirst("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier").Value;
+            var request = new RemoveCartRequest()
+            {
+                UserId = userId,
+                ProductId = productId
+            };
+            await _cartService.RemoveFromCart(request);
+        }
+
+        public async Task ClearCart()
+        {
+            var userId = User.FindFirst("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier").Value;
+            var request = new ClearCartRequest()
+            {
+                UserId = userId
+            };
+            await _cartService.ClearCart(request);
         }
     }
 }
