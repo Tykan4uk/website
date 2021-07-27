f9e5513d15c14d2e0d0a90581041ebf12ab2c397
TYKAN\Tykan <tykan4uk2010@yandex.ru>
2021-07-27 12:47:17 +0300

    Adding IOrderService, OrderService

---
+++ WebSite/WebSite/Services/Abstractions/IOrderService.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using WebSite.Models.Requests;
+using WebSite.Models.Responses;
+
+namespace WebSite.Services.Abstractions
+{
+    public interface IOrderService
+    {
+        Task<AddOrderResponse> AddOrder(AddOrderRequest addOrderRequest);
+        Task<GetByPageOrderResponse> GetByPageOrder(GetByPageOrderRequest getByPageOrderRequest);
+        Task<GetByIdOrderResponse> GetByIdOrder(GetByIdOrderRequest getByIdOrderRequest);
+    }
+}
---
+++ WebSite/WebSite/Services/OrderService.cs
@@ -0,0 +1,43 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using WebSite.Configurations;
+using WebSite.Models.Requests;
+using WebSite.Models.Responses;
+using WebSite.Services.Abstractions;
+
+namespace WebSite.Services
+{
+    public class OrderService : IOrderService
+    {
+        private readonly IHttpClientService _httpClientService;
+        private readonly RouteConfig _routeConfig;
+
+        public OrderService(IHttpClientService httpClientService, IOptions<Config> options)
+        {
+            _routeConfig = options.Value.WebSiteRoute;
+            _httpClientService = httpClientService;
+        }
+
+        public async Task<AddOrderResponse> AddOrder(AddOrderRequest addOrderRequest)
+        {
+            string url = $"{_routeConfig.Order}Add";
+            var response = await _httpClientService.SendAsync<AddOrderResponse>(url, HttpMethod.Post, addOrderRequest);
+            return response;
+        }
+
+        public async Task<GetByPageOrderResponse> GetByPageOrder(GetByPageOrderRequest getByPageOrderRequest)
+        {
+            string urlCartApi = $"{_routeConfig.Order}GetByPage";
+            var response = await _httpClientService.SendAsync<GetByPageOrderResponse>(urlCartApi, HttpMethod.Post, getByPageOrderRequest);
+            return response;
+        }
+
+        public async Task<GetByIdOrderResponse> GetByIdOrder(GetByIdOrderRequest getByIdOrderRequest)
+        {
+            string url = $"{_routeConfig.Order}GetById";
+            var response = await _httpClientService.SendAsync<GetByIdOrderResponse>(url, HttpMethod.Post, getByIdOrderRequest);
+            return response;
+        }
+    }
+}
